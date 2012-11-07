c45bb5a3d0fe36f7b16d320eb4eddb199e60c10b
Luca Fabbri <luca@keul.it>
2012-11-07 15:21:28 +0100

    documentation in rest format

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '0.4.0'
 setup(name='collective.itvalidators',
       version=version,
       description="A set of Archetypes validators for Plone, some for Italian specific needs, others useful for all",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from
       # http://pypi.python.org/pypi?:action=list_classifiers
